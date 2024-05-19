08adccaf21f8248c90cb294cbb1baf4a0f7820e7
Sumit Khanduri <sumitk@walshintegrated.com>
2024-05-19 19:15:18 -0400

    added 7 days forecast css

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -2,17 +2,19 @@ import { Component } from '@angular/core';
 import {MatButtonModule} from "@angular/material/button";
 import {MatCardModule} from "@angular/material/card";
 import {MatIconModule} from "@angular/material/icon";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
 
 @Component({
   selector: 'app-weather',
   standalone: true,
-  imports: [MatButtonModule, MatCardModule, MatIconModule],
+  imports: [MatButtonModule, MatCardModule, MatIconModule, MatFormFieldModule, MatInputModule],
   templateUrl: './weather.component.html',
   styleUrl: './weather.component.scss'
 })
 export class WeatherComponent {
 
   location = 'New Delhi';
-  temperature= '30c'
+  temperature= '30'
 
 }
